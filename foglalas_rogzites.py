92a8106dba11239db46628cc0344f7075918f3e1
Jambor92 <jambor92@gmail.com>
2022-11-30 19:50:03 +0100

    Létrehoztam egy függvényt a már meglévő foglalások mellé történő új foglalások rögzítésére, valamint a foglalt helyek újbóli foglalásának elkerülésére.

---
+++ foglalas_rogzites.py
@@ -0,0 +1,25 @@
+import hely_kivalasztas
+
+def foglalas_rogzites(foglalt_helyek):
+    
+    kilepes=0
+    duplikalt=0
+
+    while (kilepes !=1):
+        terem=int (input("Kérlek add meg a kiválasztott termet (1-3):"))
+        napszak=int (input("Kérlek add meg a kiválasztott napszakot (1-3):"))
+        sor=int (input("Kérlek add meg a kiválasztott teremben a kiválasztott ülőhely sorszámát (1-10):"))
+        oszlop=int (input("Kérlek add meg a kiválasztott teremben a kiválasztott ülőhely oszlopszámát (1-10):"))
+
+        foglalt_hely=hely_kivalasztas(terem,napszak,sor,oszlop)
+        for i in foglalt_helyek:
+            if (foglalt_hely == i):
+                print("A megadott hely már foglalt!")
+                duplikalt=1
+                break
+        if (duplikalt == 0):
+            foglalt_helyek.append(foglalt_hely)
+
+        kilepes= int(input("Szeretnél újabb foglalást rögzíteni? (Igen-0, Nem-1):"))
+
+    return foglalt_helyek
