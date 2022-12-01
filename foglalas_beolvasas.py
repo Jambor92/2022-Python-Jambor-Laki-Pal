af2da31842850867f60a4a616824a34e25dcab26
ChRuZ555 <matelaki@gmail.com>
2022-12-01 19:04:21 +0100

    Kiegészítettem a nap beolvasásához egy plusz sorral

--- foglalas_beolvasas.py
+++ foglalas_beolvasas.py
@@ -10,7 +10,7 @@ def fog_beolvasas(filename):
         for sor in befile:
             foglalt_hely=sor.strip()
             fog_lista=foglalt_hely.split(" ")
-            ulo_hely=hely_kivalasztas(int(fog_lista[0]),int(fog_lista[1]),int(fog_lista[2]),int(fog_lista[3]))
+            ulo_hely=hely_kivalasztas(int(fog_lista[0]),int(fog_lista[1]),int(fog_lista[2]),int(fog_lista[3]),int(fog_lista[4]))
             foglalas.append(ulo_hely)
 
     return foglalas
