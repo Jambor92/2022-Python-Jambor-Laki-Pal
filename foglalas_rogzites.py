eb42548d9befc3fe27622e382c2a5ddd75953e3e
ChRuZ555 <matelaki@gmail.com>
2022-12-01 19:08:36 +0100

    Módosítottam a függvényt a nap változó paraméter szerint

--- foglalas_rogzites.py
+++ foglalas_rogzites.py
@@ -1,25 +1,26 @@
 from hely_kivalasztas import hely_kivalasztas
 
+
 def fog_rogzites(foglalt_helyek):
-    
-    kilepes=0
-    duplikalt=0
+    kilepes = 0
+    duplikalt = 0
 
-    while (kilepes !=1):
-        terem=int (input("Kérlek add meg a kiválasztott termet (1-3):"))
-        napszak=int (input("Kérlek add meg a kiválasztott napszakot (1-3):"))
-        sor=int (input("Kérlek add meg a kiválasztott teremben a kiválasztott ülőhely sorszámát (1-10):"))
-        oszlop=int (input("Kérlek add meg a kiválasztott teremben a kiválasztott ülőhely oszlopszámát (1-10):"))
+    while (kilepes != 1):
+        nap = int(input("Kérlek add meg a kiválasztott napot(1-7):"))
+        terem = int(input("Kérlek add meg a kiválasztott termet (1-3):"))
+        napszak = int(input("Kérlek add meg a kiválasztott napszakot (1-3):"))
+        sor = int(input("Kérlek add meg a kiválasztott teremben a kiválasztott ülőhely sorszámát (1-10):"))
+        oszlop = int(input("Kérlek add meg a kiválasztott teremben a kiválasztott ülőhely oszlopszámát (1-10):"))
 
-        foglalt_hely=hely_kivalasztas(terem,napszak,sor,oszlop)
+        foglalt_hely = hely_kivalasztas(nap, terem, napszak, sor, oszlop)
         for i in foglalt_helyek:
             if (foglalt_hely == i):
                 print("A megadott hely már foglalt!")
-                duplikalt=1
+                duplikalt = 1
                 break
         if (duplikalt == 0):
             foglalt_helyek.append(foglalt_hely)
 
-        kilepes= int(input("Szeretnél újabb foglalást rögzíteni? (Igen-0, Nem-1):"))
+        kilepes = int(input("Szeretnél újabb foglalást rögzíteni? (Igen-0, Nem-1):"))
 
     return foglalt_helyek
