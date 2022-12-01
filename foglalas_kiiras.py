0773f24582e53a62f7c62119c5c74c6d83696a3b
ChRuZ555 <matelaki@gmail.com>
2022-12-01 19:06:59 +0100

    Kiegészítettem egy Nap(N) a kifile.write-ba

--- foglalas_kiiras.py
+++ foglalas_kiiras.py
@@ -1,6 +1,6 @@
 def fog_kiiras(fname,foglalasok):
     with open (fname,"w", encoding="utf-8") as kifile:
-        kifile.write("T;Nsz;S;O\n")
+        kifile.write("N;T;Nsz;S;O\n")
         for sor in foglalasok:
             for i in sor:
                 kifile.write((f"{i} "))
