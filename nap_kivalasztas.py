eb9b36ac9c9758d16f1971dd62f5087ce3cbd04b
Jambor92 <jambor92@gmail.com>
2022-12-01 18:42:49 +0100

    Függvény a foglalásokból egy adott napon történő foglalások számának meghatározásához.

---
+++ nap_kivalasztas.py
@@ -0,0 +1,19 @@
+def nap_kivalasztas(foglalasok,nap):
+    napok=0
+
+    for sor in foglalasok:
+            if(sor[0]==nap):
+                napok=napok+1
+    return napok
+
+
+
+
+
+
+
+
+
+
+
+
