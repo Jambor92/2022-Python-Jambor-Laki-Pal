264ee3be7f86e5745ec1ae3d5c2883dc14a4ec1a
ChRuZ555 <matelaki@gmail.com>
2022-12-01 19:10:07 +0100

    Teszteltem a nap kiválasztás függvényt

--- mozi.py
+++ mozi.py
@@ -1,10 +1,14 @@
 from foglalas_beolvasas import fog_beolvasas
 from foglalas_rogzites import fog_rogzites
 from foglalas_kiiras import fog_kiiras
+from nap_kivalasztas import nap_kivalasztas
+
 
 f_lista=[]
 uj_foglalasok=[]
 
 f_lista=fog_beolvasas("foglalas.txt")
-uj_foglalasok=fog_rogzites(f_lista)
-fog_kiiras("foglalas.txt",uj_foglalasok)
+#uj_foglalasok=fog_rogzites(f_lista)
+#fog_kiiras("foglalas.txt",uj_foglalasok)
+napok=nap_kivalasztas(f_lista,1)
+print(napok)
