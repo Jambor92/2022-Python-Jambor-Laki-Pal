cbb20fafc9767d3247fc003c8bdfacecfded4fa5
Jambor92 <jambor92@gmail.com>
2022-12-01 18:56:19 +0100

    Módosítottuk a listát egy plusz nap elem rögzítésével

--- hely_kivalasztas.py
+++ hely_kivalasztas.py
@@ -1,15 +1,15 @@
 
-def hely_kivalasztas(terem,napszak,sor,oszlop):
+def hely_kivalasztas(nap,terem,napszak,sor,oszlop):
 
     fogl_list=[]
 
+    fogl_list.append(nap)
     fogl_list.append(terem)
     fogl_list.append(napszak)
     fogl_list.append(sor)
     fogl_list.append(oszlop)
 
     return fogl_list
-
 
 
 
