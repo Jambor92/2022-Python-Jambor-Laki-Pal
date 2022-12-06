da54e8663ce6a9f068d60e173f55e48a6799ccde
Jambor92 <jambor92@gmail.com>
2022-12-06 17:53:41 +0100

    Az elkészült fő program menüpont rendszerrel

--- mozi.py
+++ mozi.py
@@ -6,9 +6,30 @@ from nap_kivalasztas import nap_kivalasztas
 
 f_lista=[]
 uj_foglalasok=[]
+lekert_napok=[]
+napi_foglalasok=[]
+menupont=0
+ossz_bevetel=0
 
-f_lista=fog_beolvasas("foglalas.txt")
-#uj_foglalasok=fog_rogzites(f_lista)
-#fog_kiiras("foglalas.txt",uj_foglalasok)
-napok=nap_kivalasztas(f_lista,1)
-print(napok)
+while (menupont !=3):
+    menupont=int (input("Kérlek válassz az alábbi menüpontok közül (Foglalás-1, Bevétel lekérdezés-2, Kilépés-3): "))
+    if (menupont ==1):
+        f_lista=fog_beolvasas("foglalas.txt")
+        uj_foglalasok=fog_rogzites(f_lista)
+        fog_kiiras("foglalas.txt",uj_foglalasok)
+    if (menupont ==2):
+        f_lista=fog_beolvasas("foglalas.txt")
+        napok=int (input("Kérlek add meg hány nap bevételét szeretnéd lekérdezni (1-7):"))
+        for i in range(napok):
+            nap=int (input(f"Kérlek add meg a(z) {i+1}. lekérdezni kívánt napot (1-7):"))
+            lekert_napok.append(nap)
+        for j in lekert_napok:
+            napi_foglalas=nap_kivalasztas(f_lista,j)
+            napi_foglalasok.append(napi_foglalas)
+        for k in range(len(lekert_napok)):
+            print(f"A {lekert_napok[k]}. napon a bevétel: {napi_foglalasok[k]*2000} Ft\n")
+            ossz_bevetel=ossz_bevetel+napi_foglalasok[k]
+        print(f"Az össz bevétel a lekérdezett napokon: {ossz_bevetel*2000} Ft\n")
+        lekert_napok.clear()
+        napi_foglalasok.clear()
+        ossz_bevetel=0
